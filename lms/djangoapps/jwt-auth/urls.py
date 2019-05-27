3e51fb469e747bd7247acdca947b0eeb83e4847d
Thinh Vo <justinvo.16.01@outlook.com>
2019-05-27 21:15:59 +0700

    Integrate jwt authentication apis

---
+++ lms/djangoapps/jwt-auth/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+
+app_name = 'jwt_auth_api'
+
+urlpatterns = [
+    url(r'^jwt_auth/refresh/', refresh_jwt_token, name='jwt_auth_api'),
+    url(r'^jwt_auth/verify/', verify_jwt_token, name='jwt-authapi'),
+    url(r'^jwt_auth/token/', obtain_jwt_token, name='jwt-auth-api'),
+]
